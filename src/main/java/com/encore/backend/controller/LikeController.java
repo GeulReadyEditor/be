ab76e041544ea8a80d4e121e676ab8b90473eb2c
Se-Hong Im <sehong1602@naver.com>
2021-08-11 14:41:58 +0900

    Add CrossOrigin

--- src/main/java/com/encore/backend/controller/LikeController.java
+++ src/main/java/com/encore/backend/controller/LikeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/{boardId}")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LikeController {
 
     LikeService likeService;
