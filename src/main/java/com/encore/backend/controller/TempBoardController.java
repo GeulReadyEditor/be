bc3ce718cd9a95fd13b6a36d202e15fdc0251509
Se-Hong Im <sehong1602@naver.com>
2021-08-11 14:44:07 +0900

    Add CrossOrigin

--- src/main/java/com/encore/backend/controller/TempBoardController.java
+++ src/main/java/com/encore/backend/controller/TempBoardController.java
@@ -13,9 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
